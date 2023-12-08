42ee57b1a4655f2da73c60c70e90861055c147a5
Marreddy Sai Nikhil Reddy <sainikhilreddym2000@gmail.com>
2023-12-07 17:34:14 -0800

    Updating experiment and best model code

--- image-emotion-classifier/helper_functions.py
+++ image-emotion-classifier/helper_functions.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import tensorflow.keras.backend as K
+import numpy as np
 
 def plot_graph(result):
     # Models Loss graph
@@ -20,6 +21,7 @@ def plot_graph(result):
     plt.legend()
     plt.show()
 
+# this is a old keras source code
 def f1_score(y_true, y_pred): 
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
@@ -28,3 +30,8 @@ def f1_score(y_true, y_pred):
     recall = true_positives / (possible_positives + K.epsilon())
     f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
     return f1_val
+
+def compute_label(label_array, class_indexes):
+    predicted_class_index = np.argmax(label_array)
+    predicted_class = list(class_indexes.keys())[predicted_class_index]
+    return predicted_class
